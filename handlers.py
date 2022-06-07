7fd95aa1fb1154733e210d3891623d37684d8134
Qusarber <sascha200116@gmail.com>
2022-06-07 17:47:43 +0300

    update and fix

--- handlers.py
+++ handlers.py
@@ -1,14 +1,24 @@
 import os
 
-from main import video_downloader
+from main import (
+    video_downloader,
+    video_quality,
+)
 from userbot import send_by_user_bot
 
 from aiogram import types
 from aiogram.dispatcher import Dispatcher, FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import (
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
+
 
 class DownloadVid(StatesGroup):
     collect_link = State()
+    collect_quality = State()
 
 
 async def process_start_command(message: types.Message):
@@ -16,11 +26,25 @@ async def process_start_command(message: types.Message):
     await message.answer("Enter url to download")
 
 
-async def process_step1(message: types.Message, state: FSMContext):
+async def process_step0(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         if message.text:
+            data['collect_link'] = message.text
+            available_resolurion = video_quality(message.text)
+            kb = ReplyKeyboardMarkup(resize_keyboard=True)
+            for i in available_resolurion:
+                kb.add(KeyboardButton(i))
+            await message.answer("Please, select quality of video", reply_markup=kb)
+            await DownloadVid.next()
+
+
+async def process_step1(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        available_resolurion = video_quality(data['collect_link'])
+        if message.text in available_resolurion:
+            kb = ReplyKeyboardRemove()
             try:
-                file = video_downloader(message.text)
+                file = video_downloader(data['collect_link'], message.text)
                 file_path_and_name = f'{file[0]}/{file[1]}'
                 await message.answer_document(document=open(file_path_and_name, 'rb'))
                 os.remove(file_path_and_name)
@@ -28,19 +52,22 @@ async def process_step1(message: types.Message, state: FSMContext):
                 data.state = None
             except:
                 try:
-                    await message.answer("We need a few more time to process your request because video size is more than 50 MB. We will send you video into private chat.")
+                    await message.answer("We need a few more time to process your request because video size is more than 50 MB. We will send you video into private chat.", reply_markup=kb)
                     await send_by_user_bot(message.from_user.id, file_path_and_name)
                     await message.answer("File was sent to your private chat")
                     os.remove(file_path_and_name)
                     os.rmdir(file[0])
                     data.state = None
                 except:
-                    await message.answer("Something went wrong. Try again via /start")
+                    await message.answer("Something went wrong. Try again via /start", reply_markup=kb)
                     os.remove(file_path_and_name)
                     os.rmdir(file[0])
                     data.state = None
+        else:
+            await message.answer("Please, select quality of video from keyboard")
 
 
 def register_handlers_core(dp: Dispatcher):
     dp.register_message_handler(process_start_command, commands=["start"])
-    dp.register_message_handler(process_step1, state=DownloadVid.collect_link)
+    dp.register_message_handler(process_step0, state=DownloadVid.collect_link)
+    dp.register_message_handler(process_step1, state=DownloadVid.collect_quality)
--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from pytube import YouTube
 import os
 
-def video_downloader(videourl):
+
+def video_quality(videourl):
+
+    yt = YouTube(videourl)
+    res = [stream.resolution for stream in yt.streams.filter(file_extension='mp4')]
+    res = list(set(list(filter(None, res))))
+    non_interested_res = ['144p', '240p', '360p', '480p']
+    for i in non_interested_res:
+        if i in res:
+            res.remove(i)
+    res.sort(key=lambda x: int(x.split('p')[0]))
+    return res
+
+
+def video_downloader(videourl, videoquality):
 
     yt = YouTube(videourl)
     path = yt.title
     try:
-        yt = yt.streams.filter(res="2160p").first()
+        yt = yt.streams.filter(res=f"{videoquality}", file_extension='mp4').first()
+        if not os.path.exists(path):
+            os.makedirs(path)
+        yt.download(path)
     except:
-        try:
-            yt = yt.streams.filter(res="1440p").first()
-        except:
-            yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-    if not os.path.exists(path):
-        os.makedirs(path)
-    yt.download(path)
-    
+        yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+        if not os.path.exists(path):
+            os.makedirs(path)
+        yt.download(path)
+
     return path, yt.default_filename
